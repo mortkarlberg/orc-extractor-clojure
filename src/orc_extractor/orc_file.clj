a1d96605c6bdc6c3314b767b9a33411e59c5aa97
Marten Karlberg <mortkarlberg@gmail.com>
2019-05-15 12:43:34 +0200

    Temp copy orc input file to learn how io works

--- src/orc_extractor/orc_file.clj
+++ src/orc_extractor/orc_file.clj
@@ -1,4 +1,11 @@
-(ns orc-extractor.orc-file)
+(ns orc-extractor.orc-file
+  (:require [clojure.java.io :as io]))
 
-(defn read-file [path]
-    (println "orc_file.read called for file:" path))
+(defn read-file [orc-file]
+  (println "orc-file.read-file called for file:" orc-file)
+  (with-open [orc-in (io/input-stream orc-file)
+              midi-out (io/output-stream (io/file "./data/extracted.mid"))]
+    (io/copy orc-in midi-out)
+))
+
+(read-file (io/file "./data/Demosong.orc"))
