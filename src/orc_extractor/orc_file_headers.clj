5d877d1275076496940c2cfcfa9af930084b0eb6
Marten Karlberg <mortkarlberg@gmail.com>
2019-06-07 19:57:36 +0200

    Start locate headers

--- src/orc_extractor/orc_file_headers.clj
+++ src/orc_extractor/orc_file_headers.clj
@@ -2,6 +2,17 @@
   (:require [orc-extractor.orc-file-constants :as orc-const]
             [orc-extractor.binary-utils :as bin-utils]))
 
+(defn locate-headers
+  "Parse all headers and return a collection of their locations."
+  [orc-file-mmap]
+  (println "hej")  
+  (let [expected-headers [orc-const/RIFF_HEADER_START
+                          orc-const/RIFF_HEADER_MIDI
+                          orc-const/RIFF_HEADER_WAVE_START
+                          orc-const/RIFF_HEADER_WAVE_DATA
+                          orc-const/RIFF_HEADER_WAVE_VOYL]]
+    (str expected-headers)))
+
 (defn parse-header
   "Parse a RIFF-header and return the length of the following data chunk."
   [header, expected-header-name]
--- test/orc_extractor/orc_file_headers_test.clj
+++ test/orc_extractor/orc_file_headers_test.clj
@@ -3,26 +3,31 @@
             [orc-extractor.orc-file-headers :as headers :refer :all]
             [orc-extractor.orc-file-constants :as orc-const :refer :all]))
 
-(deftest test-parse-header
-  (testing "headers/parse-header"
-    (testing "function callable"
-      (is function? headers/parse-header))
+(deftest test-locate-headers
+  (testing "function callable"
+    (is function? headers/locate-headers))
+  
+  (testing "prints"
+    (is (= true
+           (headers/locate-headers nil)))))
 
-    (testing "header name equals expected"
-      (is (=
+(deftest test-parse-header
+  (testing "function callable"
+    (is function? headers/parse-header))
+
+  (testing "header name equals expected"
+    (is (= 289848
            (headers/parse-header
             (seq (byte-array [0x52 0x49 0x46 0x46 0x38 0x6C 0x04 0x00]))
-            orc-const/RIFF_HEADER_START)
-           289848))
+            orc-const/RIFF_HEADER_START)))
 
-      (is (=
+    (is (= 18
            (headers/parse-header
             (seq (byte-array [0x57 0x41 0x56 0x45 0x66 0x6D 0x74 0x20 0x12 0x00 0x00 0x00]))
-            orc-const/RIFF_HEADER_WAVE_START)
-           18)))
+            orc-const/RIFF_HEADER_WAVE_START))))
 
-    (testing "header name not equal to expected"
-      (is (thrown? IllegalArgumentException
-                   (headers/parse-header
-                    (seq (byte-array [0x52 0x49 0x46 0x46 0x38 0x6C 0x04 0x00])
-                         orc-const/RIFF_HEADER_MIDI)))))))
+  (testing "header name not equal to expected"
+    (is (thrown? IllegalArgumentException
+                 (headers/parse-header
+                  (seq (byte-array [0x52 0x49 0x46 0x46 0x38 0x6C 0x04 0x00])
+                       orc-const/RIFF_HEADER_MIDI))))))
