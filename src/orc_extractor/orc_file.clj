b595ec56cbb744c7c0b866efba0162dbcaa86953
Marten Karlberg <mortkarlberg@gmail.com>
2019-06-07 12:46:00 +0200

    Clean up

--- src/orc_extractor/orc_file.clj
+++ src/orc_extractor/orc_file.clj
@@ -8,16 +8,8 @@
 
   (with-open [orc-file (clj-mmap/get-mmap filename)
               midi-out (clj-mmap/get-mmap "./data/extracted.mid")]
-
-    ;TODO instead: check all headers and save in collection with start positions/length
-    ;TODO: don't forget to handle the padding bytes
-    ;TODO: write midi to its own file
-    ;TODO: parse voyl data and use it when handling waves
-    ;TODO: write each wave to its own file, maybe with padding zeroes to push it to its correct start point in the song
-    ;TODO bonus: write a compound sequencer file from all data in some open format
     (let [header-bytes (clj-mmap/get-bytes orc-file 0 8)]
       (println (str "First 8 bytes of orc-file, " (String. header-bytes)))
-      ;(check-header header-bytes "RIFF")
     )))
 
 (defn parse-header
