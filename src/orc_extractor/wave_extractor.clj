b1392e4b874ccda9e30006c34f55d617590503e1
Marten Karlberg <mortkarlberg@gmail.com>
2019-06-28 21:27:26 +0200

    Start writing wave file

--- src/orc_extractor/wave_extractor.clj
+++ src/orc_extractor/wave_extractor.clj
@@ -4,14 +4,32 @@
             [orc-extractor.orc-file-constants :as orc-const]
             [clojure.java.io :as io]))
 
+(defn- pad-size [size]
+  (if (even? size)
+    size
+    (+ size 1)))
+
+(defn- calculate-riff-size [headers]
+  (let [fmt-header  (get headers orc-const/RIFF_HEADER_WAVE_START)
+        data-header (get headers orc-const/RIFF_HEADER_WAVE_DATA)
+        riff-size   (+ (:header-size fmt-header)
+                       (pad-size (:chunk-size fmt-header))
+                       (:header-size data-header)
+                       (pad-size (:chunk-size data-header)))]
+    ;TODO transform riff-size to little endian byte array
+    (println (byte-array [riff-size]))
+    (byte-array [riff-size])))
+
+(defn- write-riff-header [file headers]
+  (.write file orc-const/RIFF_HEADER_START)
+  (.write file (calculate-riff-size headers)))
+
 ;http://soundfile.sapp.org/doc/WaveFormat/
 (defn write-wave-file [headers orc-file-mmap orc-file-path]
   (println "Headers:" headers)
   (let [wave-file-path (str/replace orc-file-path ".orc" ".wav")]
     (with-open [file (io/output-stream wave-file-path)]
-      ;TODO calculate total riff size from fmt and data headers
-      (.write file orc-extractor.orc-file-constants/RIFF_HEADER_START)
-      ;TODO write total riff size as little endian byte array
+      (write-riff-header file headers)
       ;TODO write fmt header
       ;TODO write fmt data
       ;TODO write data header
